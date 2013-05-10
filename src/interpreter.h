813a02d036c15ad21841862cb27a7c5406ce3b64
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-05-10 16:14:22 +0200

    Document the interpreter methods

--- src/interpreter.h
+++ src/interpreter.h
@@ -5,9 +5,12 @@
  * The Lua interpreter is hidden inside this module.
  */
 
+// Opaque handle to the interpreter
 struct interpreter;
 
+// Create a lua interpreter and load the standard libraries
 struct interpreter *interpreter_create(void);
+// Destroy the lua interpreter
 void interpreter_destroy(struct interpreter *interpreter);
 
 #endif
