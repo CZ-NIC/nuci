93552853edf98ac693e1d1248ced3876f6773f3d
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-05-16 10:00:12 +0200

    Add datastore for statistics

--- src/communication.h
+++ src/communication.h
@@ -13,11 +13,15 @@ struct srv_config {
 	ncds_id config_dsid;
 	// Datastore for the configuration.
 	struct ncds_ds *config_datastore;
+	// ID of the statistics data store.
+	ncds_id stats_dsid;
+	// Datastore for the statistics
+	struct ncds_ds *stats_datastore;
 };
 
 void comm_set_print_error_callback(void(*clb)(const char *message));
-bool comm_init(const char *datastore_model_path, struct srv_config *config_out);
+bool comm_init(const char *config_model_path, const char *stats_model_path, struct srv_config *config_out);
 void comm_start_loop(const struct srv_config *config);
-void comm_cleanup(const struct srv_config *config);
+void comm_cleanup(struct srv_config *config);
 
 #endif // COMMUNICATION_H
