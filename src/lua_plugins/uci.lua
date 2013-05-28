c4aee7dc1670da8fea92f811960e3bbb844c0ff3
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-05-28 14:05:57 +0200

    List the configurations
    Not their content yet, though.

--- src/lua_plugins/uci.lua
+++ src/lua_plugins/uci.lua
@@ -1,3 +1,5 @@
+require("uci")
+
 local uci_datastore = {}
 
 function uci_datastore:set_config(config, defop, deferr)
@@ -7,8 +9,21 @@ function uci_datastore:set_config(config, defop, deferr)
 	};
 end
 
-function uci_datastore:get_config()
+local function list_config(cursor, config)
+	local result = '<config><name>' .. xml_escape(config) .. '</name>';
 
+	result = result .. '</config>';
+	return result;
+end
+
+function uci_datastore:get_config()
+	local cursor = uci.cursor()
+	local result = [[<uci xmlns='http://www.nic.cz/ns/router/uci-raw'>]];
+	for _, config in ipairs(uci_list_configs()) do
+		result = result .. list_config(cursor, config);
+	end
+	result = result .. '</uci>';
+	return result;
 end
 
 register_datastore_provider("uci.yin", uci_datastore)
