b812469214cf65475bf5c850fc40f1d25b153744
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-05-28 15:16:19 +0200

    Provide full UCI dump

--- src/lua_plugins/uci.lua
+++ src/lua_plugins/uci.lua
@@ -9,9 +9,60 @@ function uci_datastore:set_config(config, defop, deferr)
 	};
 end
 
+local function sort_by_index(input)
+	-- Sort by the value in ".index"
+	local result = {}
+	for _, value in pairs(input) do
+		table.insert(result, value);
+	end
+	table.sort(result, function (a, b)
+		if a[".type"] == b[".type"] then
+			return a[".index"] < b[".index"];
+		else
+			return a[".type"] < b[".type"];
+		end
+	end);
+	return result;
+end
+
+local function list_item(name, value)
+	local kind = 'list';
+	if type(value) ~= 'table' then
+		value = { value };
+		kind = 'option';
+	end
+	local result = '<' .. kind .. '><name>' .. xml_escape(name) .. '</name>';
+	for _, v in ipairs(value) do
+		result = result .. '<value>' .. xml_escape(v) .. '</value>';
+	end
+	result = result .. '</' .. kind .. '>';
+	return result;
+end
+
+local function list_section(section)
+	local result = '<section><type>' .. xml_escape(section[".type"]) .. "</type>";
+	if not section[".anonymous"] then
+		result = result .. "<name>" .. xml_escape(section[".name"]) .. "</name>";
+	end
+	for name, value in pairs(section) do
+		if not name:find("^%.") then -- Stuff starting with dot is special info, not values
+			result = result .. list_item(name, value);
+		end
+	end
+	result = result .. '</section>';
+	return result;
+end
+
 local function list_config(cursor, config)
 	local result = '<config><name>' .. xml_escape(config) .. '</name>';
-
+	cdata = cursor.get_all(config);
+	-- Sort the data according to their index
+	-- (this might not preserve the order between types, but at least
+	-- preserves the relative order inside one type).
+	cdata = sort_by_index(cdata);
+	for _, section in ipairs(cdata) do
+		result = result .. list_section(section);
+	end
 	result = result .. '</config>';
 	return result;
 end
