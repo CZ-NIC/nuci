2b060f8b4a1aab2f6f4ca9882c5de937fd18584c
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-06-03 16:33:07 +0200

    Identify the correct nodes to perform operations on
    Search the model XML and the current config for the nodes that match the
    one in the command that is being handled at the moment.

--- src/lua_lib/editconfig.lua
+++ src/lua_lib/editconfig.lua
@@ -1,10 +1,27 @@
+-- Compare names and namespaces of two nodes
+local function cmp_elemname(command_node, config_node, ns)
+	local name1, ns1 = command_node:name();
+	local name2, ns2 = config_node:name();
+	ns1 = ns1 or ns; -- The namespace may be missing from the command XML, as it is only snippet
+	return name1 == name2 and ns1 == ns2;
+end
+-- Compare names, namespaces and their text
+local function cmp_name_content(command_node, config_node, ns)
+	return cmp_elemname(command_node, config_node, ns) and command_node:text() == config_node:text();
+end
 -- Names of valid node names in model, with empty data for future extentions
 local model_names = {
-	leaf={},
-	['leaf-list']={},
-	container={},
-	list={}
+	leaf={ cmp=cmp_elemname },
+	['leaf-list']={ cmp=cmp_name_content },
+	container={ cmp=cmp_elemname },
+	list={
+		cmp=function(node1, node2)
+			error("Comparing list not implemented yet");
+			-- TODO: Get the keys and compare them.
+		end
+	}
 }
+
 local netconf_ns = 'urn:ietf:params:netconf:base:1.0'
 local yang_ns = 'urn:ietf:params:xml:ns:yang:yin:1'
 
@@ -20,10 +37,10 @@ local function model_identify(model_dir, node_name)
 		local name, ns = model:name();
 		-- The corrent namespace and attribute
 		if ns == yang_ns and model:attribute('name') == node_name then
-			for model_name in pairs(model_names) do
+			for model_name, model_opts in pairs(model_names) do
 				-- Found allowed name, all done
 				if name == model_name then
-					return model;
+					return model, model_opts;
 				end
 			end
 			-- Not a valid name. Try next node.
@@ -31,12 +48,23 @@ local function model_identify(model_dir, node_name)
 	end
 end
 
+-- Look through the config and try to find a node corresponding to the command_node one. Consider the model.
+local function config_identify(model_node, model_opts, command_node, config, ns)
+	local cmp_func = model_opts.cmp;
+	for node in config:iterate() do
+		if cmp_func(command_node, node, ns) then
+			return node;
+		end
+	end
+	-- It is OK not to find, returning nothing then.
+end
+
 -- Perform operation on all the children here.
 local function children_perform(config, command, model, ns, defop, errop, ops)
 	for command_node in command:iterate() do
 		local command_name, command_ns = command_node:name();
 		if command_ns == ns then
-			model_node = model_identify(model, command_name);
+			local model_node, model_opts = model_identify(model, command_name);
 			if not model_node then
 				-- TODO What about errop = continue?
 				return {
@@ -46,7 +74,10 @@ local function children_perform(config, command, model, ns, defop, errop, ops)
 				};
 			end
 			print("Found model node " .. model_node:name() .. " for " .. command_name);
+			local config_node = config_identify(model_node, model_opts, command_node, config, ns);
+			print("Found config node " .. config_node:name() .. " for " .. command_name);
 		end
+		-- TODO Should we ignore items of different namespace (as per XML recommendations) or report it as unknown namespace?
 	end
 end
 --[[
