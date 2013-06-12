85cd1cc4b7011e39f5e3fd1ccaec0d85ffbfa8d4
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-06-12 16:49:46 +0200

    Connect to NUCI, show received data when it comes

--- tools/test_gui/config.cpp
+++ tools/test_gui/config.cpp
@@ -1,5 +1,73 @@
 #include "config.h"
 
-Config::Config() {
+#include <QProcess>
+
+#include <cassert>
+#include <cstdio>
+
+#define MARK "]]>]]>"
+
+Config::Config() :
+	process(NULL)
+{
 	setupUi(this);
+}
+
+void Config::on_connectButton_clicked() {
+	if (process) {
+		disconnectNuci();
+	} else {
+		connectNuci();
+	}
+}
+
+void Config::connectNuci() {
+	incoming.clear();
+	// Create and connect the process
+	process = new QProcess(this);
+	connect(process, SIGNAL(readyReadStandardOutput()), this, SLOT(data()));
+	connect(process, SIGNAL(finished(int, QProcess::ExitStatus)), this, SLOT(terminated()));
+	process->start(commandEdit->text());
+	// Let it start
+	process->waitForStarted(-1);
+	// Push the <hello> message there.
+	sendData("<?xml version='1.0' encoding='UTF-8'?><hello xmlns='urn:ietf:params:xml:ns:netconf:base:1.0'><capabilities><capability>urn:ietf:params:netconf:base:1.0</capability></capabilities></hello>");
+	connectButton->setEnabled(false);
+	connectButton->setText("Disconnect");
+}
+
+void Config::disconnectNuci() {
+
+}
+
+void Config::data() {
+	// Some data arrived from remote
+	incoming += process->readAll();
+	int index;
+	while ((index = incoming.indexOf(MARK)) != -1) {
+		// Extract the message and drop it from the front
+		QByteArray message(incoming.data(), index);
+		incoming.remove(0, index + strlen(MARK));
+		printf("Received XML:\n%s\n\n", message.data());
+
+
+	}
+}
+
+void Config::terminated() {
+	// The remote terminated
+}
+
+void Config::sendData(const QString &data) {
+	QByteArray cp(data.toLocal8Bit());
+	printf("Sending XML message:\n%s\n\n", cp.data());
+	cp.append("\n" MARK);
+	while (!cp.isEmpty())
+		writeData(cp);
+}
+
+void Config::writeData(QByteArray &array) {
+	qint64 written = process->write(array);
+	assert(written > 0);
+	array.remove(0, written);
 }
--- tools/test_gui/config.h
+++ tools/test_gui/config.h
@@ -5,9 +5,23 @@
 
 #include "ui_config.h"
 
+class QProcess;
+
 class Config : public QMainWindow, private Ui::Config {
+	Q_OBJECT;
 public:
 	Config();
+private slots:
+	void on_connectButton_clicked();
+	void data();
+	void terminated();
+private:
+	void connectNuci();
+	void disconnectNuci();
+	void sendData(const QString &data);
+	void writeData(QByteArray &data);
+	QProcess *process;
+	QByteArray incoming;
 };
 
 #endif
