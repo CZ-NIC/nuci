09caa83c754fd053e18e039ee4ee310f9b8a63c6
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-06-14 15:37:51 +0200

    Make more buttons work

--- tools/test_gui/config.h
+++ tools/test_gui/config.h
@@ -18,6 +18,9 @@ private slots:
 	void on_connectButton_clicked();
 	void on_downloadButton_clicked();
 	void on_removeButton_clicked();
+	void on_mergeButton_clicked();
+	void on_replaceButton_clicked();
+	void on_createButton_clicked();
 	void on_configView_clicked();
 	void data();
 	void terminated();
@@ -31,6 +34,7 @@ private:
 	void handleRpc(const QDomDocument &rpc);
 	size_t sendRpc(const QString &xml, RpcCallback callback = NULL);
 	void configDownloaded(const QDomDocument &rpc, size_t id);
+	void prepareXml(const QString &operation, bool subnodes, bool content);
 	QProcess *process;
 	QByteArray incoming;
 	QHash<size_t, RpcCallback> rpcCallbacks;
