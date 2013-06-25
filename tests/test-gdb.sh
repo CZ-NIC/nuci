ea717e7840e39fb818f58ddd9a27b102a92f83c7
Robin Obůrka <robin.oburka@nic.cz>
2013-06-25 14:49:24 +0200

    Test file for manual gdb debugging provided.

---
+++ tests/test-gdb.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+export NUCI_TEST_CONFIG_DIR="./example-config"
+
+(
+read
+
+cat <<ENDXML
+<?xml version="1.0" encoding="UTF-8"?>
+<hello xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
+  <capabilities>
+    <capability>urn:ietf:params:netconf:base:1.0</capability>
+  </capabilities>
+</hello>
+]]>]]><?xml version='1.0' encoding='UTF-8'?><rpc xmlns='urn:ietf:params:xml:ns:netconf:base:1.0' message-id='4'><edit-config>
+    <target>
+        <running/>
+    </target>
+    <config></config>
+</edit-config>
+</rpc>
+]]>]]>
+ENDXML
+
+read
+) | socat STDIO EXEC:./bin/nuci,pty
