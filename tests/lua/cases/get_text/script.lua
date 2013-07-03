411ff00e3223ac3a411cbed7723d618664b9defb
Robin Obůrka <robin.oburka@nic.cz>
2013-07-03 09:03:32 +0200

    [xmlwrap] Test for get_text has one more case.

--- tests/lua/cases/get_text/script.lua
+++ tests/lua/cases/get_text/script.lua
@@ -1,13 +1,27 @@
 
 local doc = xmlwrap.read_memory("<elem>text <!-- comment --> some text before CDATA <![CDATA[ I'm Pretty cool example of CDATA ]]> and some more text</elem>");
 
-io.stdout:write("Original XML: " .. doc:strdump() .. "\n");
+io.stdout:write("Original XML (1): " .. doc:strdump() .. "\n");
 
 local node = doc:root();
 
 local str = node:text();
 if (str == nil) then
-	io.stdout:write("Obtained text: is nil value\n");
+	io.stdout:write("Obtained text (1): is nil value\n");
 else
-	io.stdout:write("Obtained text: " .. node:text() .. "\n");
+	io.stdout:write("Obtained text (1): " .. node:text() .. "\n");
+end
+
+
+doc = xmlwrap.read_memory("<root> BEFORE ELEM <elem>text <!-- comment --> some text before CDATA <![CDATA[ I'm Pretty cool example of CDATA ]]> and some more text</elem> AFTER ELEM </root>");
+
+io.stdout:write("Original XML (2): " .. doc:strdump() .. "\n");
+
+node = doc:root();
+
+str = node:text();
+if (str == nil) then
+	io.stdout:write("Obtained text (2): is nil value\n");
+else
+	io.stdout:write("Obtained text (2): " .. node:text() .. "\n");
 end
