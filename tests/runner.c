2bdc0c01060645c8c21512646d1da70f8cc077b6
Robin Obůrka <robin.oburka@nic.cz>
2013-07-04 10:01:54 +0200

    Runner has cleanup phases of used libs for better memory debug

--- tests/runner.c
+++ tests/runner.c
@@ -3,13 +3,22 @@
 #include <lua.h>
 #include <lualib.h>
 #include <lauxlib.h>
+#include <libxml/parser.h>
+#include <libxml/tree.h>
 
 static lua_State *lua;
 
 int main(int argc, const char *argv[]) {
 	(void) argc;
+
+	//libxml2 init
+	xmlInitParser();
+		LIBXML_TEST_VERSION
+
+	//lua init
 	lua = luaL_newstate();
 	luaL_openlibs(lua);
+
 	xmlwrap_init(lua);
 	for (const char **arg = argv + 1; *arg; arg ++) {
 		fprintf(stderr, "Running file %s\n", *arg);
@@ -19,5 +28,13 @@ int main(int argc, const char *argv[]) {
 			return 1;
 		}
 	}
+
+	//lua cleanup
+	lua_close(lua);
+
+	//libxml2 cleanup
+	xmlCleanupParser();
+	xmlMemoryDump();
+
 	return 0;
 }
