643b2a152dfbe1ffae8f0803d20a6871e88f647e
Robin Obůrka <robin.oburka@nic.cz>
2013-08-28 17:40:01 +0200

    Add nethist plugin

---
+++ src/lua_plugins/nethist.lua
@@ -0,0 +1,75 @@
+require("uci");
+require("datastore");
+
+local HIST_FILE = "/tmp/nethist";
+
+-- trim whitespace from right end of string
+local function trimr(s)
+	return s:find'^%s*$' and '' or s:match'^(.*%S)'
+end
+
+local function parse_line(line)
+	local items = {};
+	local i = 1;
+	line = line .. ',';
+	for item in line:gmatch('([^,]*)[,]') do
+		items[i] = item;
+		i = i + 1;
+	end
+
+	return items;
+end
+
+local function parse_file(file, node)
+	local prev_time = -1; -- 0 is possible value; not 1. 1. 1970 but unsnapped slot
+
+	local snap_node;
+	for line in file:lines() do
+		items = parse_line(line);
+		--[[snap_node = node:add_child('items');
+		for _,item in pairs(items) do
+			snap_node:add_child('item'):set_text(item);
+		end]]
+		if prev_time ~= items[1] then
+			snap_node = node:add_child('snapshot'):set_attribute('time', items[1]);
+		end
+
+		if items[2] == "cpu" then
+			snap_node:add_child('cpu'):add_child('load'):set_text(items[3]);
+		elseif items[2] == "memory" then
+			local mem_node = snap_node:add_child('memory');
+			mem_node:add_child('memtotal'):set_text(items[3]);
+			mem_node:add_child('memfree'):set_text(items[4]);
+			mem_node:add_child('buffers'):set_text(items[5]);
+			mem_node:add_child('cached'):set_text(items[6]);
+		elseif items[2] == "network" then
+			local net_node = snap_node:add_child('network');
+			net_node:set_attribute('interface', items[3]);
+			net_node:add_child('rx'):set_text(items[4]);
+			net_node:add_child('tx'):set_text(items[5]);
+		end
+
+		prev_time = items[1];
+	end
+end
+
+local datastore = datastore('nethist.yin')
+
+function datastore:get()
+	local doc, root, node;
+
+	--prepare XML subtree
+	doc = xmlwrap.new_xml_doc("nethist", "http://www.nic.cz/ns/router/nethist");
+	root = doc:root();
+
+	local file = io.open(HIST_FILE);
+	if not file then
+		return nil, "Cannot open file with history";
+	end
+	parse_file(file, root:add_child('snapshots'));
+	file:close();
+
+	return doc:strdump();
+end
+
+register_datastore_provider(datastore)
