037086307fd71f9d572d0fd5e5eb77e48023eb40
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-09-05 10:37:27 +0200

    Compute the registration serial
    Replace the fixed string with a computed one based on the output of
    atsha204cmd.

--- src/lua_plugins/registration.lua
+++ src/lua_plugins/registration.lua
@@ -1,9 +1,22 @@
 require("datastore");
+require("nutils");
 
 local datastore = datastore('registration.yin');
 
 function datastore:get()
-	local serial = "xyz";
+	local ecode, serial_output, errs = run_command(nil, 'atsha204cmd', 'serial-number');
+
+	if ecode ~= 0 then
+		return nil, "Failed to acquire own serial number: " .. errs;
+	end
+
+	local ecode, challenge_output, errs = run_command('0000000000000000000000000000000000000000000000000000000000000000', 'atsha204cmd', 'challenge-response');
+
+	if ecode ~= 0 then
+		return nil, "Failed to compute the serial extention: " .. errs;
+	end
+
+	local serial = trimr(serial_output) .. "-" .. trimr(challenge_output);
 
 	-- Encode it as XML
 	local doc, root, node;
