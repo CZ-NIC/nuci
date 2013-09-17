90d229a9fbad73abc5a776a10663275d9ad41bff
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-09-17 14:12:56 +0200

    Move plugin to plugins
    We now support bare plugins, so no need to call it a library any more.

--- src/lua_plugins/supervisor_test.lua
+++ src/lua_plugins/supervisor_test.lua
@@ -1,14 +1,6 @@
 require("datastore");
 require("views_supervisor");
 
--- OK, this is very stupid and I know it
--- Our buildsystem is not prepared for this concept.
--- Files in src/lua plugins are autoloaded but they require YIN model.
--- We need some directory with autoloading and without any more checks.
--- This is only temporaly hack
-require("ap_testing1");
-
-
 local datastore = datastore('supervisor_test.yin')
 
 function datastore:get()
