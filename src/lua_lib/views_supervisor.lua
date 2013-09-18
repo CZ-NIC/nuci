50b289a4ed5d0b36e4f0a4983a712e4bdf124ac5
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-09-18 15:29:10 +0200

    Rewrite of the supervisor
    When trying to understand the code in supervisor and cleaning it up, it
    ended up as mostly complete rewrite inspired by the original code.
    
    Significant changes:
    • No support for requesting value from given plugin. This was actually
      an anti-feature, as plugin should never need to know about
      existence of other plugin.
    • No support for requesting value (yet, TBD).
    • Allows generating of multiple trees/models. This is needed feature and
      was neglected in the original implementation.
    • Split of registration of plugin and its position from actually getting
      the values. This is a separate operation conceptually and the code
      should follow, for better readability.
    • Do not register the indices with paths. Path is a plugin trait, while
      the indices are traits of values. Therefore, paths should be
      registered at start-up, while indices should be provided when we get
      the values.
    • Remove double callbacks (supervisor calling a plugin, which in turns
      calls the supervisor). It was confusing and not necessary.
    
    Drawbacks:
    • Still doesn't handle collisions (but marks them for future
      processing).
    • Several TODOs and FIXMEs in the code.

--- src/lua_lib/views_supervisor.lua
+++ src/lua_lib/views_supervisor.lua
@@ -1,240 +1,277 @@
 require("tableutils");
 require("nutils");
 require("datastore");
+require("xmltree");
 
 -- Global state varables
 supervisor = {
 	plugins = {},
-	tree = {},
-	cached = false,
-	doc = nil
+	tree = { subnodes = {}, plugins = {} },
 };
 
-dbg = "";
-local function dbg_add(str)
-	dbg = dbg .. str .. "\n";
+-- Add a plugin to given path in the tree
+local function register_to_tree(tree, path, plugin)
+	local node = tree;
+	for _, level in ipairs(path) do
+		local sub = node.subnodes[level] or { plugins = {}, subnodes = {} };
+		node.subnodes[level] = sub;
+		node = sub;
+	end
+	table.insert(node.plugins, plugin);
 end
 
-local function tree_try_add_key(keys, keyset)
-	local function exists_keyset(keys, keyset)
-		for _, keys_internal in pairs(keys) do
-			if match_keysets(keys_internal, keyset) then
-				-- Found one that matches
-				return true;
+--[[
+Find the list of plugins at given path in the tree.
+If not found, returns empty set.
+
+Takes the '*' terminal label into account.
+
+Frow every plugin, only its first instance in the list is preserved
+(so a plugin will never be present twice).
+]]
+local function callbacks_find(tree, path)
+	local pre_result = {};
+	local node = tree;
+	for _, level in ipairs(path) do
+		-- If there's a .* here, add it to the result
+		table.insert(pre_result, (node.subnodes["*"] or {}).plugins or {});
+		node = node.subnodes[level] or { subnodes = {} };
+	end
+	-- Add the final node's plugins
+	table.insert(pre_result, node.plugins or {});
+	local result = {};
+	-- Flatten the result to a single list
+	for _, partial in ipairs(pre_result) do
+		table.extend(result, partial);
+	end
+	return table.uniq(result);
+end
+
+--[[
+Register a plugin. It'll be inserted into the tree for callbacks.
+]]
+function supervisor:register_plugin(plugin)
+	table.insert(self.plugins, plugin);
+	for _, path in pairs(plugin:positions()) do
+		register_to_tree(self.tree, path, plugin);
+	end
+end
+
+--[[
+Get list of plugins that are valid for given path.
+]]
+function supervisor:get_plugins(path)
+	return callbacks_find(self.tree, path);
+end
+
+-- Can't use the local function syntax, due to mutual dependency with merge_data
+local build_children;
+
+--[[
+Take the values and convert them to a tree (eg. compact the common beginnings
+of paths), creating a table-built tree. The tree is compatible with the
+xmltree library.
+]]
+local function merge_data(values, level)
+	local level = level or 1; -- If not provided, we start from the front
+
+	--[[
+	We first divide the values by their name in path on the current level.
+	We have some names for the children and we set the ones wich end here
+	]]
+	local children_defs = {};
+	local local_defs = {};
+	for _, value in ipairs(values) do
+		local name = value.path[level];
+		if name then
+			-- Get the data gathered for the child or a new list
+			local def = children_defs[name] or {};
+			children_defs[name] = def;
+			table.insert(def, value);
+		else
+			table.insert(local_defs, value);
+		end
+	end
+	--[[
+	Now we have definitions for all the children, so we handle them
+	(indirectly) recursively and concatenate the results together.
+	Beware that there may be multiple results from one name of child.
+	]]
+	local children = {};
+	local child_error;
+	for name, values in pairs(children_defs) do
+		local children_local, err = build_children(name, values, level);
+		table.extend(children, children_local);
+		if err then
+			child_error = true;
+		end;
+	end
+	-- Extract the local information to something more digestible
+	local seen_multival;
+	local multivals = {};
+	local seen_val;
+	local vals = {};
+	local err_val;
+	for _, value in ipairs(local_defs) do
+		if value.multival then
+			seen_multival = true;
+			table.insert(multivals, value.multival);
+		else
+			seen_val = true;
+			if next(vals) and not value.val then
+				err_val = true;
 			end
-		end
-		return false;
-	end
-
-	if keyset ~= nil then
-		if not exists_keyset(keys, keyset) then
-			table.insert(keys, keyset);
+			table.insert(vals, value.val);
 		end
 	end
-end
-
-local function tree_rec_add(node, level, plugin, path, key)
-	-- Test path variable
-	if path[level] == nil then
-		return nil, "Empty path";
+	-- Check for error conditions
+	if seen_val and seen_multival then -- Both single and multi value?
+		err_val = true;
 	end
-
-	-- Create if node is empty
-	if table.is_empty(node) then
-		node.name = path[level];
-		node.keys = {};
-		node.children = {};
-	end
-
-	-- Add keys
-	tree_try_add_key(node.keys, key[level]);
-
-	-- Recurse or create node
-	if path[level+1] == nil then
-		-- I'm leaf
-		if node.plugins == nil then
-			node.plugins = {}; -- Table of plugin doesn't exists yet - create it
-		end
-		-- Leaf needs infromation about registered plugins
-		table.insert(node.plugins, plugin);
-
-	else
-		-- I'm inner node
-		if node.children[path[level+1]] == nil then
-			node.children[path[level+1]] = {}; -- Table of child doesn't exists yet - create it
-		end
-
-		tree_rec_add(node.children[path[level+1]], level+1, plugin, path, key);
-	end
-end
-
-function supervisor:register_ap(plugin)
-	supervisor.plugins[plugin.id] = plugin;
-end
-
-function supervisor:register_value(plugin, path, key)
-	tree_rec_add(supervisor.tree, 1, plugin, path, key);
-end
-
-function supervisor:register_all_values()
-	local ret, err;
-	for _, plugin in pairs(supervisor.plugins) do
-		ret, err = plugin:register_values();
-		if not ret then
-			return ret, err;
+	if seen_val then
+		local prev; -- All shall be the same
+		for _, val in ipairs(vals) do
+			if prev and prev ~= val then
+				err_val = true;
+			end
+			prev = val;
 		end
 	end
-
-	return true;
-end
-
--- FIXME: This probably doesn't work with multiple datastore providers. Or does it?
-function supervisor:init(init_doc)
-	if supervisor.doc == nil then
-		supervisor.doc = init_doc;
-	end
-end
-
-function supervisor:invalidate_cache()
-	supervisor.cached = false;
-end
-
-local function build_get_value(plugins, path, level, keyset)
-	-- FIXME: Returning first valid answer is temporaly solution
-	for _, plugin in pairs(plugins) do
-		local res = plugin:get(path, level, keyset);
-		if res ~= nil then
-			return res;
-		end
-	end
-
-	return nil;
-end
-
-local function build_rec(node, onode, keyset, path, level)
-	if node == nil then
-		return
-	end
-
-	path[level] = node.name;
-
-	local new_node;
-	if table.is_empty(node.keys) then
-		if table.is_empty(node.children) then
-			-- Generate leaf's value
-			local res = build_get_value(node.plugins, path, level, keyset);
-			if res ~= nil then
-				for _, val in ipairs(res) do
-					new_node = onode:add_child(node.name);
-					new_node:set_text(val);
+	if seen_multival then
+		local prev; -- All shall be the same, but we don't care about the order
+		for _, mval in ipairs(multivals) do
+			if prev then
+				-- Little bit of abuse, but it works.
+				if not match_keysets(list2map(prev), list2map(mval)) then
+					err_val = true;
 				end
 			end
-		else
-			-- Recurse to children
-			new_node = onode:add_child(node.name);
-			for _, child in pairs(node.children) do
-				build_rec(child, new_node, keyset, path, level+1);
-			end
+			prev = mval;
+		end
+	end
+	-- Build the nodes
+	local function build_result(value)
+		local result = { children = children, text = value };
+		local errors = {};
+		if child_error then
+			table.insert(errors, 'children');
+		end
+		if err_val then
+			table.insert(errors, 'value');
+		end
+		if next(errors) then
+			result.errors = errors;
+			result.source = values;
+		end
+		return result;
+	end
+	local result = {};
+	if seen_multival then
+		for _, val in ipairs(multivals[1]) do
+			table.insert(result, build_result(val));
 		end
 	else
-		for _, key in pairs(node.keys) do
-			-- Create copy of this node with this keyset in XML
-			new_node = onode:add_child(node.name);
-			-- Add keyset record into XML node
-			for key_name, key_val in pairs(key) do
-				new_node:add_child(key_name):set_text(key_val);
+		table.insert(result, build_result(vals[1]));
+	end
+	return result;
+end
+
+build_children = function(name, values, level)
+	local result = {};
+	while next(values) do -- Pick a keyset, filter it out and process
+		local keyset = (values[1].keys or { [level] = {} })[level];
+		local picked, rest = {}, {};
+		local key_list = {};
+		-- FIXME: Check the key sets are for the same indexes
+		-- FIXME: Choose order of the keys
+		for name, value in pairs(keyset) do
+			table.insert(key_list, { name = name, text = value });
+		end
+		for _, value in ipairs(values) do
+			if match_keysets(keyset, (value.keys or { [level] = {} })[level]) then
+				table.insert(picked, value);
+			else
+				table.insert(rest, value);
 			end
-			-- Recurse to children of this copy
-			for _, child in pairs(node.children) do
-				--table.insert(key, keyset);
-				build_rec(child, new_node, key, path, level+1);
+			values = rest;
+			local generated = merge_data(picked, level + 1);
+			for _, child in pairs(generated) do
+				local children = {};
+				-- The keys must go first
+				table.extend(children, key_list);
+				table.extend(children, child.children or {});
+				child.children = children;
 			end
+			table.extend(result, generated);
 		end
 	end
+	for _, child in pairs(result) do
+		child.name = name;
+	end
+	return result;
 end
 
-function supervisor:build_tree(onode)
-	build_rec(supervisor.tree, onode, nil, {}, 1);
-end
+function supervisor:get(name, ns)
+	if not self.cached then
+		-- Nothing ready yet. Build the complete tree and store it.
 
-function supervisor:get()
-	-- Does some data exist?
-	if supervisor.cached == true then
-		return supervisor.doc;
-	end
-	-- Data doesn't exists
-
-	-- First of all - let plugins to register all values of tree
-	ret, err = supervisor:register_all_values();
-	if not ret then
-		return ret, err;
-	end
-
-	-- Build new tree
-	supervisor.cached = true;
-	supervisor:build_tree(supervisor.doc:root());
-
-	-- Development and debug
-	--dbg_add(table.tostring(supervisor.tree)); -- This output is very long - uncomment it if you need it
-	supervisor.doc:root():add_child("dbg"):set_text(dbg);
-
-
-	return supervisor.doc;
-end
-
-local function get_plugins_rec(node, path, level)
-	if node == nil then
-		return nil;
-	end
-
-	if node.name == path[level] then
-		if path[level+1] == nil then
-			return node.plugins;
+		-- First, let each plugin dump everything and store it for now.
+		local values = {};
+		for _, plugin in ipairs(self.plugins) do
+			local pvalues, errors = plugin:get();
+			if errors then
+				return nil, errors;
+			end
+			-- We call the get() without path and keys, to get everything
+			for _, value in pairs(pvalues) do
+				value.from = plugin; -- Remember who provided this ‒ for debugging and tracking of differences in future
+			end
+			table.extend(values, pvalues);
 		end
-		if not table.is_empty(node.children) then
-			return get_plugins_rec(node.children[path[level+1]], path, level+1);
+		-- Go through the values and merge them together, in preorder DFS
+		self.data = merge_data(values)[1]; -- There must be exactly 1 result at the top level
+		-- Index the direct sub-children, for easier lookup.
+		-- We assume there's just single one of each name.
+		self.index = {}
+		for _, subtree in pairs(self.data.children or {}) do
+			self.index[subtree.name] = subtree;
 		end
+		--[[
+		TODO: Collision and error checking ‒ walk the tree and call relevant plugins
+		on the places where something happens.
+		]]
+		self.cached = true;
 	end
+	--[[
+	Extract the appropriate part of tree and convert to XML.
 
-	return nil;
+	First, take the part of the tree by the name, set up the namespace and
+	run it through the xmltree module.
+	]]
+	local subtree = self.index[name] or { name = name };
+	subtree.namespace = ns;
+	return xmltree_dump(subtree);
 end
 
-function supervisor:get_plugins(path)
-	return get_plugins_rec(supervisor.tree, path, 1);
-end
-
--- Get value calculated by more plugins
-function supervisor:get_value(path, keyset)
-	local plugins = supervisor:get_plugins(path);
-	if plugins == nil then
-		return nil;
-	end
-
-	return build_get_value(plugins, path, #path, keyset);
-end
-
--- Get value from single plugin
-function supervisor:get_raw_value(id, path, keyset)
-	local plugin = supervisor.plugins[id];
-	if plugin == nil then
-		return nil;
-	end
-
-	return plugin:get(path, #path, keyset);
+--[[
+Invalidate cache, most useful after a complete get or editconfig request.
+]]
+function supervisor:invalidate_cache()
+	self.cached = nil;
+	self.data = nil;
+	self.index = nil;
 end
 
 --[[
 Generate a usual datasource provider based on the supervisor and views.
 Pass the yin file, the rest is extracted from it.
 ]]
-
 function supervisor:generate_datasource_provider(yin_name)
 	local datastore = datastore(yin_name);
+	local supervisor = self;
 
 	function datastore:get()
-		supervisor:init(xmlwrap.new_xml_doc(self.model_name, self.model_ns));
-
-		local doc, err = supervisor:get();
+		local doc, err = supervisor:get(self.model_name, self.model_ns);
 
 		if doc then
 			return doc:strdump();
--- src/lua_plugins/ap_testing1.lua
+++ src/lua_plugins/ap_testing1.lua
@@ -1,7 +1,7 @@
 require("views_supervisor");
 require("nutils");
 
-local self = { id = "ap_testing1" };
+local self = {};
 -- Consider this example
 --[[
 <networking xmlns="http://www.nic.cz/ns/router/networking">
@@ -11,30 +11,24 @@ local self = { id = "ap_testing1" };
 			<address>10.0.0.2</address>
 			<gateway>10.0.0.1</gateway>
 			<dns>10.0.0.1</dns>
-			<nat>true</nat>
-			<nat6>false</nat6>
+			<nat/>
 		</interface>
 		<interface>
 			<name>eth1</name>
 			<address>10.0.1.5</address>
 			<gateway>10.0.1.1</gateway>
 			<dns>10.0.1.1</dns>
-			<nat>false</nat>
-			<nat6>false</nat6>
 		</interface>
 	</internet>
 	<lan>
 		<interface>
 			<name>eth2</name>
 			<address>192.168.1.1</address>
-			<dhcp>true</dhcp>
-			<bridge>none</bridge>
+			<dhcp/>
 		</interface>
 		<interface>
 			<name>eth3</name>
 			<address>192.168.1.2</address>
-			<dhcp>false</dhcp>
-			<bridge>none</bridge>
 		</interface>
 	</lan>
 </networking>
@@ -43,102 +37,82 @@ local self = { id = "ap_testing1" };
 -- Some dummy testing stuff
 self.watch = {
 	{
-		path = {'networking', 'internet', 'interface', 'address'},
-		key = {nil, nil, {["name"] = "eth0", ["xyz"] = "abcd"}, nil},
-		devvals = {"10.11.12.13", "10.11.12.14", "10.11.12.15"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'address'},
+		keys = {{}, {}, {}, {name = "eth0", xyz = "abcd"}, {}},
+		multival = {"10.11.12.13", "10.11.12.14", "10.11.12.15"}
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'gateway'},
-		key = {nil, nil, {["name"] = "eth0"}, nil},
-		devvals = {"10.11.12.1"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'gateway'},
+		keys = {{}, {}, {}, {name = "eth0"}, {}},
+		val = "10.11.12.1"
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'dns'},
-		key = {nil, nil, {["name"] = "eth0"}, nil},
-		devvals = {"10.11.12.2"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'dns'},
+		keys = {{}, {}, {}, {name = "eth0"}, {}},
+		multival = {"10.11.12.2"}
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'nat'},
-		key = {nil, nil, {["name"] = "eth0"}, nil},
-		devvals = {"true"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'nat'},
+		keys = {{}, {}, {}, {name = "eth0"}, {}}
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'nat6'},
-		key = {nil, nil, {["name"] = "eth0"}, nil},
-		devvals = {"false"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'address'},
+		keys = {{}, {}, {}, {name = "eth1"}, {}},
+		multival = {"10.11.22.13"}
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'address'},
-		key = {nil, nil, {["name"] = "eth1"}, nil},
-		devvals = {"10.11.22.13"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'gateway'},
+		keys = {{}, {}, {}, {name = "eth1"}, {}},
+		val = "10.11.22.1"
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'gateway'},
-		key = {nil, nil, {["name"] = "eth1"}, nil},
-		devvals = {"10.11.22.1"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'dns'},
+		keys = {{}, {}, {}, {name = "eth1"}, {}},
+		multival = {"10.11.22.2"}
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'dns'},
-		key = {nil, nil, {["name"] = "eth1"}, nil},
-		devvals = {"10.11.22.2"}
+		path = {'supervisor-test', 'networking', 'internet', 'interface', 'nat'},
+		keys = {{}, {}, {}, {name = "eth1"}, {}}
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'nat'},
-		key = {nil, nil, {["name"] = "eth1"}, nil},
-		devvals = {"true"}
+		path = {'supervisor-test', 'networking', 'lan', 'interface', 'address'},
+		keys = {{}, {}, {}, {name = "eth3"}, {}},
+		multival = {"192.168.1.1"}
+	},
+	{ -- WTF? Like, which DHCP server we should ask? The sole purpose of DHCP is not to configure anything!
+		path = {'supervisor-test', 'networking', 'lan', 'interface', 'dhcp'},
+		keys = {{}, {}, {}, {name = "eth3"}, {}},
+		val = "192.168.1.0"
 	},
 	{
-		path = {'networking', 'internet', 'interface', 'nat6'},
-		key = {nil, nil, {["name"] = "eth1"}, nil},
-		devvals = {"false"}
-	},
-	{
-		path = {'networking', 'lan', 'interface', 'address'},
-		key = {nil, nil, {["name"] = "eth3"}, nil},
-		devvals = {"192.168.1.1"}
-	},
-	{
-		path = {'networking', 'lan', 'interface', 'dhcp'},
-		key = {nil, nil, {["name"] = "eth3"}, nil},
-		devvals = {"192.168.1.0"}
-	},
-	{
-		path = {'networking', 'lan', 'interface', 'address'},
-		key = {nil, nil, {["name"] = "eth4"}, nil},
-		devvals = {"192.168.1.1"}
+		path = {'supervisor-test', 'networking', 'lan', 'interface', 'address'},
+		keys = {{}, {}, {}, {name = "eth4"}, {}},
+		multival = {"192.168.1.1"}
 	}
 }
 
-function self:register_values()
-	for _, item in pairs(self.watch) do
-		supervisor:register_value(self, item.path, item.key);
+--[[
+Return paths in which I'm interested.
+
+FIXME:
+This is a bit broken, since we return a path multiple times. The supervisor can handle that, but it is
+not nice. Probably OK for test plugin.
+]]
+function self:positions()
+	local result = {};
+	for _, value in ipairs(self.watch) do
+		table.insert(result, value.path);
 	end
-
-	return true;
-end;
-
-function self:get(path, level, keyset)
-	local match;
-	for _, item in pairs(self.watch) do
-		match = true;
-		for i, node in ipairs(item.path) do
-			if node ~= path[i] then
-				match = false
-				break;
-			end
-		end
-		if match == true then
-			for _, key in pairs(item.key) do
-				if key ~= nil then
-					if match_keysets(key, keyset) then
-						return item.devvals;
-					end
-				end
-			end
-		end
-	end
-
-	return nil;
+	return result;
 end
 
-supervisor:register_ap(self);
+--[[
+Return values. Parameters are ignored here and we return everything all the time.
+This is legal, the parameters are optimisation only and the caller would filter
+it anyway.
+]]
+function self:get(path, level, keyset)
+	return self.watch;
+end
+
+supervisor:register_plugin(self);
