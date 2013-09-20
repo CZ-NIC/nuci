a646d0313ee840f07117697cf00f645c8b00722a
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-09-20 10:28:01 +0200

    Further supervisor test utilities
    • Creation of small test plugins
    • Registering of given plugins
    • Comparing of values with recursion through tables (and failing test if
      they don't equal).
    
    This is for the #2677, but will be useful for further tests too.
    
    Refs: #2677, #2681, #2683, #2686.

--- tests/supervisor_test.lua
+++ tests/supervisor_test.lua
@@ -5,11 +5,64 @@ Tests for the views supervisor.
 
 We have several auxiliary help functions here. Each test is a function, registered
 in a table. Then, the main routine runs each in turn and checks if they terminate
-correctly. Then it invalidates the cache in the supervisor after each test.
+correctly. Then it invalidates the cache and resets in the supervisor after each test.
 ]]
 
 package.path = 'src/lua_lib/?.lua;' .. package.path;
 require("views_supervisor");
+require("dumper");
+
+--[[
+Generate a provider plugin, based on bunch of value definitions. Nothing too fancy.
+]]
+local function test_provider(value_definitions)
+	local provider = {};
+	-- Extract the paths, to know where to register.
+	function provider:positions()
+		local result = {};
+		for _, value in ipairs(value_definitions) do
+			table.insert(result, value.path);
+		end
+		return result;
+	end
+	return provider;
+end
+
+--[[
+Check the value1 and value2 are the same. Recurses through table structures,
+to check them for deep equality.
+]]
+local function deep_equal(value1, value2)
+	local type1, type2 = type(value1), type(value2);
+	if type1 ~= type2 then
+		return false;
+	end
+	if type1 == 'table' then
+		-- Shortcut, if they are the same address
+		if value1 == value2 then return true; end
+		local function check(t1, t2)
+			for key, value in pairs(t1) do
+				if not deep_equal(value, t2[key]) then
+					return false;
+				end
+			end
+			return true;
+		end
+		return check(value1, value2) and check(value2, value1);
+	else
+		return value1 == value2;
+	end
+end
+
+local function test_equal(expected, real, message)
+	message = message or 'Values differ';
+	if not deep_equal(expected, real) then
+		error(message .. ': ' .. DataDumper({
+			expected = expected,
+			real = real
+		}));
+	end
+end
 
 local tests = {
 	{
@@ -26,11 +79,17 @@ local tests = {
 
 local function run_test(test)
 	io.write('Running test "', test.name, "\"\t");
-	-- TODO: Setup
+	-- The provider plugins
+	for _, plugin in ipairs(test.provider_plugins or {}) do
+		supervisor:register_plugin(plugin);
+	end
 	io.write("Setup\t");
-	test.body();
+	test.body(test);
 	io.write("Body\t");
+	-- Reset the cache
 	supervisor:invalidate_cache();
+	supervisor.plugins = {};
+	supervisor.tree = { subnodes = {}, plugins = {} }
 	io.write("OK\n");
 end
 
