9222e3c7a1268bfce06fe2f846bef5fa8f929862
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-10-02 11:02:51 +0200

    Generate registration code based on challenge

--- src/lua_plugins/registration.lua
+++ src/lua_plugins/registration.lua
@@ -3,27 +3,20 @@ require("nutils");
 
 local datastore = datastore('registration.yin');
 
-function datastore:get()
-	local ecode, serial_output, errs = run_command(nil, 'atsha204cmd', 'serial-number');
-
-	if ecode ~= 0 then
-		return nil, "Failed to acquire own serial number: " .. errs;
+function datastore:user_rpc(rpc)
+	if rpc == 'get' then
+		local ecode, stdout, stderr = run_command(nil, 'sh', '-c', 'curl --cacert /etc/ssl/vorner.pem https://test-dev.securt.cz/challenge.cgi | atsha204cmd challenge-response');
+		if ecode ~= 0 then
+			return nil, "Can't generate challenge";
+		end
+		local ecode, serial = run_command(nil, 'atsha204cmd', 'serial-number');
+		if ecode ~= 0 then
+			return nil, "Can't get my own serial";
+		end
+		return "<reg-num xmlns='" .. self.model_ns .. "'>" .. trimr(serial) .. "-" .. trimr(stdout) .. "</reg-num>";
+	else
+		return nil, 'TODO:  Proper error message';
 	end
-
-	local ecode, challenge_output, errs = run_command('0000000000000000000000000000000000000000000000000000000000000000', 'atsha204cmd', 'challenge-response');
-
-	if ecode ~= 0 then
-		return nil, "Failed to compute the serial extention: " .. errs;
-	end
-
-	local serial = trimr(serial_output) .. "-" .. trimr(challenge_output);
-
-	-- Encode it as XML
-	local doc, root, node;
-	doc = xmlwrap.new_xml_doc("registration", self.model_ns);
-	root = doc:root();
-	root:add_child("serial"):set_text(serial);
-	return doc:strdump();
 end
 
 register_datastore_provider(datastore)
