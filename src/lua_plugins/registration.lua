ad631f44ef9b90ed56d207117cb0e991e3961354
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-10-02 11:18:08 +0200

    Tweak downloading of the challenge
    Don't check the certificate. It is not needed here and we don't want to
    depend on the certificate file here.
    
    Use a variable with the url.

--- src/lua_plugins/registration.lua
+++ src/lua_plugins/registration.lua
@@ -3,9 +3,20 @@ require("nutils");
 
 local datastore = datastore('registration.yin');
 
+-- Where we get the challenge
+local challenge_url = 'https://test-dev.securt.cz/challenge.cgi';
+
 function datastore:user_rpc(rpc)
 	if rpc == 'get' then
-		local ecode, stdout, stderr = run_command(nil, 'sh', '-c', 'curl --cacert /etc/ssl/vorner.pem https://test-dev.securt.cz/challenge.cgi | atsha204cmd challenge-response');
+		--[[
+		Download the challenge and generate a response to it.
+
+		Notice that the challenge doesn't check certificate here. It is simply not needed.
+		If someone is able to MITM the communication, they'd be able to only prevent
+		generation of the correct response, but that would be possible with the
+		cert checking too.
+		]]
+		local ecode, stdout, stderr = run_command(nil, 'sh', '-c', 'curl -k ' .. challenge_url .. ' | atsha204cmd challenge-response');
 		if ecode ~= 0 then
 			return nil, "Can't generate challenge";
 		end
