16fb1d7dc8d5d6a45109d5279e76562a58cb3713
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-10-23 15:12:13 +0200

    Make start of updater non-blocking
    The command is only to start it, not to wait for the finish.

--- src/lua_plugins/updater.lua
+++ src/lua_plugins/updater.lua
@@ -69,7 +69,7 @@ end
 
 function datastore:user_rpc(rpc)
 	if rpc == 'check' then
-		local code, stdout, stderr = run_command(nil, 'updater.sh');
+		local code, stdout, stderr = run_command(nil, 'sh', '-c', 'updater.sh &');
 		if code ~= 0 then
 			return nil, "Failed to run the updater: " .. stderr;
 		end
