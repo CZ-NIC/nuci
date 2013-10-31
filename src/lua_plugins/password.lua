54898e4d5bdc7af638b17019011c7ccf0cf0cfcc
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2013-10-31 16:42:35 +0100

    Be able to set passwords

--- src/lua_plugins/password.lua
+++ src/lua_plugins/password.lua
@@ -8,6 +8,30 @@ function datastore:user_rpc(rpc, data)
 
 	if rpc == 'set' then
 		nlog(NLOG_DEBUG, "Setting password");
+		local password_node = find_node_name_ns(root, 'password', self.model_ns);
+		if not password_node then
+			return nil, {
+				msg = "Missing the <password> parameter, don't know what to use as password",
+				app_tag = 'data-missing',
+				info_badelem = 'password',
+				info_badns = self.model_ns
+			};
+		end
+		local user_node = find_node_name_ns(root, 'user', self.model_ns);
+		if not user_node then
+			return nil, {
+				msg = "Missing the <user> parameter, don't whose password to set",
+				app_tag = 'data-missing',
+				info_badelem = 'user',
+				info_badns = self.model_ns
+			};
+		end
+		local input = password_node:text() .. "\n";
+		input = input .. input;
+		local ecode, stdout, stderr = run_command(input, 'passwd', user_node:text());
+		if ecode ~= 0 then
+			return nil, "Failed to set password: " .. stderr;
+		end
 	else
 		return nil, {
 			msg = "Command '" .. rpc .. "' not known",
