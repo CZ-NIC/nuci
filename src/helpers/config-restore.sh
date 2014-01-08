0d6caa4d130625c0664d51c11dd7efde827ade73
Michal 'vorner' Vaner <michal.vaner@nic.cz>
2014-01-08 16:36:26 +0100

    The update helper

---
+++ src/helpers/config-restore.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# Copyright 2014, CZ.NIC z.s.p.o. (http://www.nic.cz/)
+#
+# This file is part of NUCI configuration server.
+#
+# NUCI is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# NUCI is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with NUCI.  If not, see <http://www.gnu.org/licenses/>.
+
+set -e
+
+cd /
+base64 -d | bzip -cd | tar xp
+
+# Run the updater and reboot in the background
+(
+	updater.sh -n
+	reboot
+) & >/dev/null 2>&1 &
+
+# Wait for the updater to start up, then we can terminate.
+while [ '!' -d /tmp/update-state ] ; do
+	:
+done
