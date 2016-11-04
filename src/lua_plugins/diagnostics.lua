ae10fab5e0df3bf1be9a82eca9381e7c7539551c
Stepan Henek <stepan.henek@nic.cz>
2016-11-04 14:54:47 +0100

    diagnostics: update according to the latests specifications
    * new rpc list-diagnostics and remove-diagnostics added
    * get-prepared rpc revorked (returns gzipped output)

--- src/lua_plugins/diagnostics.lua
+++ src/lua_plugins/diagnostics.lua
@@ -24,22 +24,19 @@ local datastore = datastore("diagnostics.yin");
 
 local binary_path = "/usr/share/diagnostics/diagnostics.sh";
 
-function read_modules(stdout, root, dir_path)
-	local finished_modules = {};
-	for line in lines(stdout) do
-		if not line:match("\.preparing$") then
-			local file = io.open(dir_path .. '/' .. line);
-			if file then
-				local node = root:add_child("module");
-				-- module.out -> module
-				node:add_child("name"):set_text(line:match("(.*)\.out"));
-				node:add_child("output"):set_text(file:read("*all"));
-				file:close();
-				table.insert(finished_modules, line);
-			end
+function get_diag_id(root, ns)
+		-- get the generated diagnostics
+		local diag_id_node = find_node_name_ns(root, 'diag-id', ns);
+		if diag_id_node then
+			return diag_id_node:text();
+		else
+			return nil, {
+				msg = "Missing the mandatory <diag-id> parameter.",
+				app_tag = 'data-missing',
+				info_badelem = 'diag-id',
+				info_badns = self.model_ns
+			};
 		end
-	end
-	return finished_modules;
 end
 
 function datastore:user_rpc(rpc, data)
@@ -48,6 +45,7 @@ function datastore:user_rpc(rpc, data)
 
 	if rpc == 'prepare' then
 		-- generate the diagnostics
+
 		local modules = {};
 		for mid in root:iterate() do
 			local name, ns = mid:name();
@@ -60,8 +58,8 @@ function datastore:user_rpc(rpc, data)
 		-- get random id
 		math.randomseed(os.time());
 		local diag_id = string.format("%08x", math.random(1, 0x7fffffff));
-		local diag_dir_path = '/tmp/diagnostics-' .. diag_id;
-		local ecode, stdout, stderr = run_command(nil, binary_path, '-b', '-O', diag_dir_path, unpack(modules));
+		local diag_path = '/tmp/diagnostics-' .. diag_id .. '.out';
+		local ecode, stdout, stderr = run_command(nil, binary_path, '-b', '-o', diag_path, unpack(modules));
 		if ecode ~= 0 then
 			return nil, "Error performing diagnostics: " .. stderr;
 		end
@@ -74,12 +72,12 @@ function datastore:user_rpc(rpc, data)
 
 	elseif rpc == 'get-prepared' then
 		-- get the generated diagnostics
-		local diag_id_node = find_node_name_ns(root, 'diag-id', self.model_ns);
-		local diag_id = diag_id_node:text();
-		-- trim id
-		diag_id = diag_id:match("^%s*(.*[^%s])%s*$") or ""
 
-		local diag_dir_path = '/tmp/diagnostics-' .. diag_id;
+		local diag_id, err = get_diag_id(root, self.model_ns);
+		if not diag_id then
+			return nil, err;
+		end
+		local diag_path = '/tmp/diagnostics-' .. diag_id .. '.out';
 
 		-- response xml
 		local new_xml = xmlwrap.new_xml_doc(self.model_name, self.model_ns);
@@ -87,34 +85,31 @@ function datastore:user_rpc(rpc, data)
 		local status_node = new_root:add_child("status");
 
 		-- first try to read directory.preparing
-		local ecode, stdout, stderr = run_command(nil, 'ls', '-1', diag_dir_path .. '.preparing/');
-		if ecode ~= 0 then
-			-- try to read the final directory
-			local ecode, stdout, stderr = run_command(nil, 'ls', '-1', diag_dir_path .. '/');
+		local file = io.open(diag_path);
+		if file then
+			local data = file:read("*all");
+			local ecode, stdout, stderr = run_command(data, 'gzip')
 			if ecode ~= 0 then
-				status_node:set_text('missing');
-			else
-				status_node:set_text('ready');
-				read_modules(stdout, new_root, diag_dir_path);
-				-- remove the entire directory after complete read
-				run_command(nil, 'rm', '-rf', diag_dir_path);
+				return nil, "Error diagnostics packing diagnostics: " .. stderr;
 			end
+			ecode, stdout, stderr = run_command(stdout, 'base64')
+			if ecode ~= 0 then
+				return nil, "Error diagnostics base64 encoding failed: " .. stderr;
+			end
+			new_root:add_child("output"):set_text(stdout);
+			status_node:set_text('ready');
 		else
-			-- read the preparing directory
-			status_node:set_text('preparing');
-			local finished = read_modules(stdout, new_root, diag_dir_path .. '.preparing/');
-			-- remove the output of processed modules from both dicrectory and directory.preparing
-			for _, module_path in pairs(finished) do
-				run_command(nil, 'rm', '-rf',
-					diag_dir_path .. '/' .. module_path,
-					diag_dir_path .. '.preparing/' .. module_path
-				);
+			if io.open(diag_path .. '.preparing') then
+				status_node:set_text('preparing');
+			else
+				status_node:set_text('missing');
 			end
 		end
 
 		return new_xml:strdump();
 
 	elseif rpc == 'list-modules' then
+		-- list all modules
 
 		-- response xml
 		local new_xml = xmlwrap.new_xml_doc(self.model_name, self.model_ns);
@@ -129,6 +124,51 @@ function datastore:user_rpc(rpc, data)
 		end
 
 		return new_xml:strdump();
+
+	elseif rpc == 'list-diagnostics' then
+		-- list generated diagnostics
+
+		-- response xml
+		local new_xml = xmlwrap.new_xml_doc(self.model_name, self.model_ns);
+		local new_root = new_xml:root();
+
+		local ecode, stdout, stderr = run_command(nil, "sh", "-c", "ls -1 /tmp/diagnostics-*");
+		if ecode == 0 then
+			-- some diagnostics found
+			for line in lines(stdout) do
+				local status = nil;
+				local match = line:match("diagnostics%-(.*)%.out%.preparing$");
+				if match then
+					status = 'preparing';
+				else
+					match = line:match("diagnostics%-(.*)%.out$");
+					if match then
+						status = 'ready';
+					end
+				end
+				if match then
+					-- insert node
+					local node = new_root:add_child("diagnostic");
+					node:add_child("diag-id"):set_text(match);
+					node:add_child("status"):set_text(status);
+				end
+			end
+		end
+
+		return new_xml:strdump();
+
+	elseif rpc == 'remove-diagnostic' then
+		-- remove store diagnostic
+		local diag_id, err = get_diag_id(root, self.model_ns);
+		if not diag_id then
+			return nil, err;
+		end
+
+		local ecode, _, stderr = run_command(nil, "rm", "/tmp/diagnostics-" .. diag_id .. ".out");
+		if ecode ~= 0 then
+			return nil, "Error deleting diagnostics: " .. stderr;
+		end
+		return '<ok/>';
 	end
 
 end
